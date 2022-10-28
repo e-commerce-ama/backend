8905e7a6420f3cf2a69205b0bdf58df04081599f
a.safa <amisfaking@gmail.com>
2022-10-28 22:48:05 +0330

    fixed toggle port on intellij reset project(this will to delete in future)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from 'dotenv'
 import bodyParser from "body-parser";
 import routes from './routes/routes.js'
+import killProcess from 'kill-port'
 
 const app = express();
 app.use(bodyParser.json({limit: "32mb", extended: true}));
@@ -15,11 +16,13 @@ dotenv.config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const db = mongoose
+        await db.connect(process.env.MONGO_URI);
         app.listen(process.env.PORT, () => {
             console.log(`Server Running On Port: ${process.env.PORT}`)
         }).on('error', async (e) => {
-            if (e.code === "EADDRINUSE") await mongoose.connection.close()
+            if (e.code === "EADDRINUSE")
+                await killProcess(process.env.PORT, 'tcp')
         })
     } catch (err) {
         console.error(`Connection Failed!`, err.message)
--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -3,9 +3,9 @@ import User from "../../models/user/user.js"
 const getUser = async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-        res.status('200').json(user)
+        res.status(200).json(user)
     } catch (e) {
-        res.status('404').json({message: e.message})
+        res.status(404).json({message: e.message})
     }
 }
 
