2d63d787983f3715e5d48c45dbd9434a3aa4f301
a.safa <amisfaking@gmail.com>
2022-11-26 11:40:07 +0330

    initialize nest js

--- routes/user/routes.js
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import getUser from "../../controllers/user/userController.js";
-
-const userRoutes = Router();
-
-userRoutes.get('/user/:id', getUser)
-
-export default userRoutes
