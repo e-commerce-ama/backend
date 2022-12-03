a52dbf3c8cee7f0bf00b8a7d9eb0077c52546db3
a.safa <amisfaking@gmail.com>
2022-12-03 16:19:18 +0330

    init models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/e-commerce'), UserModule, ProductModule],
+})
 export class AppModule {}
