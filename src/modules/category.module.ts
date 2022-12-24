1e178108655daa915c61d068d15e27ce772f81c0
a.safa <amisfaking@gmail.com>
2022-12-24 19:25:09 +0330

    added category to product

--- src/modules/category.module.ts
+++ src/modules/category.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from '../services/category.service';
 import { CategoryController } from '../controllers/category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from '../schemas/category.schema';
 
 @Module({
   providers: [CategoryService],
   controllers: [CategoryController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema, collection: 'category' },
+    ]),
+  ],
 })
 export class CategoryModule {}
