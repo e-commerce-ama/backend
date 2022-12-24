473a314c20b3cba3e1ab303b5bfa9852bf3cc465
a.safa <amisfaking@gmail.com>
2022-12-24 20:26:50 +0330

    first phase of email confirtmation

---
+++ src/services/mail.service.ts
@@ -0,0 +1,23 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { User } from '../schemas/user.schema';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(user: User, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      template: './confirmation',
+      context: {
+        name: `${user.first_name} ${user.last_name}`,
+        url,
+      },
+    });
+  }
+}
